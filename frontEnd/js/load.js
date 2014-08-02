5e3543917654b853ab82c9f71e70c435caac332e
bodhi <bodhi@comp.nus.edu.sg>
2014-08-02 16:01:09 +0800

    commented a whole bunch of JavaScript. Because my JavaScript wasn't inspiring enough.

--- frontEnd/js/load.js
+++ frontEnd/js/load.js
@@ -1,36 +1,36 @@
 var canvas;
 var context;
-var HEIGHT=600, WIDTH=1200;
-var dx=60,dy=30;
-var x=200,y=60;
-var runAnimation=true;
-var bbby=100;
-var bbbx=WIDTH/2-50;
-var flag=1;
-var name=document.cookie.split('=')[1].toUpperCase();
-var ip='';
+var HEIGHT=25, WIDTH=40;
+var dx=32,dy=32;
+// var x=200,y=60;
+// var runAnimation=true;
+// var bbby=100;
+// var bbbx=WIDTH/2-50;
+// var flag=1;
+// var name=document.cookie.split('=')[1].toUpperCase();
+// var ip='';
 
 window.addEventListener('keydown',performKeyDownEvent,true);
 
-function getBBBX(){
-	return bbbx;
-}
-
-function getBBBY(){
-	if (bbby==HEIGHT-200)
-		flag=-1;
-	else if (bbby<0)
-		flag=1;
-	if (flag==1)
-		bbby+=20;
-	else
-		bbby-=20;
-	return bbby;
-}
+// function getBBBX(){
+// 	return bbbx;
+// }
+// 
+// function getBBBY(){
+// 	if (bbby==HEIGHT-200)
+// 		flag=-1;
+// 	else if (bbby<0)
+// 		flag=1;
+// 	if (flag==1)
+// 		bbby+=20;
+// 	else
+// 		bbby-=20;
+// 	return bbby;
+// }
 
 function startView(){
 	if(document.cookie.split('=')[1]){
-		name=document.cookie.split(';')[0].split('=')[1];
+// 		name=document.cookie.split(';')[0].split('=')[1];
 		canvas=document.getElementById("myCanvas");
 		context=canvas.getContext('2d');
 		var ip='';
@@ -45,18 +45,18 @@ function draw() {
 	if (runAnimation){
 		context.beginPath();
 		context.clearRect(0, 0, WIDTH, HEIGHT);
-		
+		drawALL(); 
 		// draw BBB block
-		context.fillStyle = 'red';
-		context.font="20px Serif bold";
-		context.fillText('Burning Box', getBBBX()+15, getBBBY()+90);
-
-		context.rect(getBBBX(), getBBBY(), 130,220);
-		
-		
-		context.strokeStyle = 'black';
-		context.stroke();
-		context.closePath();
+		// context.fillStyle = 'red';
+// 		context.font="20px Serif bold";
+// 		context.fillText('Burning Box', getBBBX()+15, getBBBY()+90);
+// 
+// 		context.rect(getBBBX(), getBBBY(), 130,220);
+// 		
+// 		
+// 		context.strokeStyle = 'black';
+// 		context.stroke();
+// 		context.closePath();
 		// context.beginPath();
 // 		context.arc(WIDTH/2 - 60, HEIGHT/2, 50,0,2*Math.PI);
 // 		context.fillStyle = '#00FFFF';
@@ -67,7 +67,7 @@ function draw() {
 // 		context.closePath();
 		// Draw players
 
-		drawALL(); 
+// 		drawALL(); 
 
 		//drawOthers(); 
 		
@@ -105,40 +105,40 @@ function drawALL(){
 // }
 
 // 
-function drawEach(){
-
-// 	console.log(x,y);
-	context.beginPath();
-// 	context.rect(x, y, Math.round(context.measureText(name).width)+10, 25);
-// 	context.arc(x,y,context.measureText(name).width/2+10,0,2*Math.PI);
-// 	context.fillStyle = '#FFFFFF';
-// 	context.fill();
-// 	context.lineWidth = 3;
-// 	context.strokeStyle = colorStroke;
-// 	context.stroke();
-// 	context.closePath();
+// function drawEach(){
+// 
+// // 	console.log(x,y);
 // 	context.beginPath();
-// 	if (document.cookie.split('=')[1].toUpperCase()!=name){
-// 		context.font="17px Serif";
-// 		context.fillStyle='#000000';
-// 	}
-// 	else{
-// 		context.font="20px Serif";
-// 		context.fillStyle='#FF0000';
-// 	}
-// 	context.fillText(name, x-Math.round(context.measureText(name).width/2), y+5);
-	
-	context.closePath();
-	
-}
+// // 	context.rect(x, y, Math.round(context.measureText(name).width)+10, 25);
+// // 	context.arc(x,y,context.measureText(name).width/2+10,0,2*Math.PI);
+// // 	context.fillStyle = '#FFFFFF';
+// // 	context.fill();
+// // 	context.lineWidth = 3;
+// // 	context.strokeStyle = colorStroke;
+// // 	context.stroke();
+// // 	context.closePath();
+// // 	context.beginPath();
+// // 	if (document.cookie.split('=')[1].toUpperCase()!=name){
+// // 		context.font="17px Serif";
+// // 		context.fillStyle='#000000';
+// // 	}
+// // 	else{
+// // 		context.font="20px Serif";
+// // 		context.fillStyle='#FF0000';
+// // 	}
+// // 	context.fillText(name, x-Math.round(context.measureText(name).width/2), y+5);
+// 	
+// 	context.closePath();
+// 	
+// }
 
-function drawOthers(names){
-// draw others as and when they join the game
-	for (var i =0; i < names.length; i++){
-		// draw the others
-		// drawEach(names[i], 'white', 'black');
-	}
-}
+// function drawOthers(names){
+// // draw others as and when they join the game
+// 	for (var i =0; i < names.length; i++){
+// 		// draw the others
+// 		// drawEach(names[i], 'white', 'black');
+// 	}
+// }
 
 function performKeyDownEvent(event){
 	switch (event.keyCode){
